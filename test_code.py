0dbb66d17140c95363ca96d837d6982971145b60
Niklas <grethlen@dhbw-loerrach.de>
2021-04-22 14:00:33 +0200

    Create test_code.py
    Created a test codefile, which should work with the provided environment.

---
+++ test_code.py
@@ -0,0 +1,12 @@
+from bertopic import BERTopic
+from sklearn.datasets import fetch_20newsgroups
+print("import success")
+
+docs = fetch_20newsgroups(subset='test',  remove=('headers', 'footers', 'quotes'))['data']
+
+topic_model = BERTopic()
+print("going to transform")
+topics, _ = topic_model.fit_transform(docs)
+print("transform success")
+print(topic_model.get_topic_freq().head())
+print(topic_model.get_topic(43))
