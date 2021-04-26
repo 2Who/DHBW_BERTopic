d8d9840fb8ec707a938a0e3f073ef2791dfcd9ec
Niklas <grethlen@dhbw-loerrach.de>
2021-04-26 15:29:31 +0200

    Update csv_bert.py
    Renamed some functions. The csv file can now successfully be fed into the transform function. There is still an error while transforming the input.

--- csv_bert.py
+++ csv_bert.py
@@ -12,13 +12,14 @@ print("import success")
 def main():
     # Old way to create document from 20newsgroups
     # docs = fetch_20newsgroups(subset='test',  remove=('headers', 'footers', 'quotes'))['data']
+    # print(len(docs))
 
     document = CreateDocFromCSV()
-    # bertModel = BERTopic()
-    # print("going to transform")
-    # topics, _ = bertModel.fit_transform(document)
-    # print("transform success")
-    # Showing_Topics(bertModel)
+    bertModel = BERTopic()
+    print("going to transform")
+    topics, _ = bertModel.fit_transform(document)
+    print("transform success")
+    ShowTopics(bertModel)
 
 
 #--------------------------------------------------------
@@ -28,23 +29,23 @@ def CreateDocFromCSV():
     # reading from csv file
     dirname = os.path.dirname(__file__)
     pathToFile = os.path.join(dirname + "/", "Trainingsdaten" + "/", "data.csv")
-    print(pathToFile)
+    # print(pathToFile)
 
-    df = pd.read_csv(pathToFile)
-    # print(df.values)
-    docs = list(df.loc["url"].values)
+    dataFrame = pd.read_csv(pathToFile)
+    docs = dataFrame['body'].tolist()
+    # docs = list(df.loc["url"].values)
     # docs = list(df.loc['url', 'title', 'pub_date', 'lead', 'body', 'crawl_date', 'language', 'images', 'tags', 'source', 'elastic_id', 'authors', 'number_of_comments', 'sections', 'edition', 'pub_date_short', 'year_month'].values)
     print("document created")
     return docs
 
-def Showing_Topics(myModel):
+def ShowTopics(myModel):
     print(myModel.get_topic_freq().head())
     print(myModel.get_topic_freq())
     print("1st topics: " + myModel.get_topic(0)) 
     print("3rd topics: " + myModel.get_topic(2))
     print("42nd topics: " + myModel.get_topic(42))
 
-def Visualize_Topics(myModel):
+def VisualizeModel(myModel):
     myModel.visualize_topics()
 
 if __name__ == '__main__':
