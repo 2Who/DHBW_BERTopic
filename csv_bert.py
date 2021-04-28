9ae115389e69bc05491a140199a3da1a7a4b9dbf
Niklas <grethlen@dhbw-loerrach.de>
2021-04-28 14:27:20 +0200

    Changed file name

--- csv_bert.py
+++ csv_bert.py
@@ -21,7 +21,7 @@ def main():
     # transformedModel = TransformModel(csvDocument)
 
     # import an existing model
-    transformedModel = BERTopic.load(PROGRAM_PATH + "/models/vanilla_deutsch.BERT")
+    transformedModel = BERTopic.load(PROGRAM_PATH + "/models/vanilla_deutsch")
     ShowTopics(transformedModel)
 
     # ShowTopics(transformedModel)
